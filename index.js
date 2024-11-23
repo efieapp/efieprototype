318841bdee30dedc7629ce8b537743d79966e387
Game Zone <102915160+gamezoneofficial@users.noreply.github.com>
2024-11-23 15:27:27 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const loadPage = function(){
+    let loadr = document.querySelector(".loadpage");
+    let loader = document.createElement("span");
+    loader.classList.add('spinner');
+    loadr.appendChild(loader);
+    loadr.innerText = "Efie N'Fie Property Management Company.";
+    
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+import { loadPage } from ".https://gamezoneofficial.github.io/Afieapp/index.js";
+
+window.addEventlistener("load", (e) => {
+    e.preventDefault();
+    
+    loadPage()
+});
+
