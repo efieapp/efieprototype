e75a30dfe993429be3bad86b3a4722e42d1c7627
Game Zone <102915160+gamezoneofficial@users.noreply.github.com>
2024-11-24 05:56:24 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { loadPage } from ".https://gamezoneofficial.github.io/Afieapp/index.js";
+import { loadPage } from 'https://gamezoneofficial.github.io/Afieapp/index.js';
 
 window.addEventlistener("load", (e) => {
     e.preventDefault();
