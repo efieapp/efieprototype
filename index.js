8e27544ecb987c272e68492addf3c85de37ec23d
Efie N'Fie Property Management LLC <102915160+efieapp@users.noreply.github.com>
2024-11-29 23:55:37 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const loadPage = function(){
     let loadr = document.querySelector("#loadpage");
     loadr.innerText = "Efie N'Fie Property Management Company.";
-    window.location.assign("https://gamezoneofficial.github.io/Afieapp/home.html")
+    const loadEfie = "https://efieapp.github.io/efieprototype/home.html";
+    window.open(loadEfie,"_self","location=false,status=true")
+    status = "Efie N'Fie Property Management Company";
 }
 
 export { loadPage };
