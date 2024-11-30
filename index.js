1263c139b8f94a05453a340d44daf9c887913f2a
Efie N'Fie Property Management LLC <102915160+efieapp@users.noreply.github.com>
2024-11-30 00:34:14 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const loadPage = function(){
     let loadr = document.querySelector("#loadpage");
     loadr.innerText = "Efie N'Fie Property Management Company.";
     const loadEfie = "https://efieapp.github.io/efieprototype/home.html";
-    window.open(loadEfie,"_self","location=false,status=true")
+    window.open(loadEfie,"_blank","location=false,status=true")
     status = "Efie N'Fie Property Management Company";
+    if(location.href = "https://efieapp.github.io/efieprototype/index.html"){
+        window.close();
+    }
 }
 
 export { loadPage };
