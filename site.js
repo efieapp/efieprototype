9bde1d60a7b334db75c7ad886e520db7286071b8
Efie N'Fie Property Management LLC <102915160+efieapp@users.noreply.github.com>
2024-12-06 17:47:09 +0000

    Add files via upload

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
-import { loadPage } from ''https://efieapp.github.io/efieprototype/index.js';
+import { loadPage } from 'https://efieapp.github.io/efieprototype/index.js';
 
 window.addEventListener("load", (e) => {
     e.preventDefault();
